6e7331c4173ab79075195990fc7385e0686a72a6
howardzhangcs <howardzhang@college.harvard.edu>
2015-04-12 18:13:02 -0400

    q4

--- cs186-mturk-intro.js
+++ cs186-mturk-intro.js
@@ -2,14 +2,13 @@
 
 // TO-DO:  Find the functions to print your balance and the # of open hits.
 print("Hello MTurk!")
-print("Your balance is: " + <YOUR CODE>  )
-print("No. of hits: "+ <YOUR CODE> )
+print("Your balance is: " + mturk.getAccountBalance())
+print("No. of hits: "+mturk.getHITs().length)
 
 // Explore the HIT object. 
 // Get the first HIT and then iterate over its properties.
 if(mturk.getHITs().length>0) {
-	var hitObj = <YOUR CODE> 
+	var hitObj = mturk.getHITs()[0]
 	for(attr in hitObj) 
 		print("Attr ="+attr+ " val="+hitObj[attr])
 }
-
