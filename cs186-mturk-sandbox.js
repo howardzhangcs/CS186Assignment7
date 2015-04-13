6640f84dc399ba706adbecdd7b3fdec48229b6d3
Peter Bang <pbang@college.harvard.edu>
2015-04-13 16:58:27 -0400

    new sandbox

--- cs186-mturk-sandbox.js
+++ cs186-mturk-sandbox.js
@@ -8,12 +8,22 @@ var page = createWebpageFromTemplate(
  <input type="submit" value="Submit"></input>
 </div>);
 
+function makeSecure(url) {
+    return url.substring(0, 4) + "s" + url.substring(4);
+}
+
 // TO-DO define the HIT parameters.
-var hitParams = <YOUR CODE>
+var hitParams = {
+    title: "Random Number",
+    description: "Select a random number",
+    url: makeSecure(page),
+    height: 800,
+    reward: 0.02,
+    autoApprovalDelayInSeconds: 60
+}
 
 // Create the HIT
 var hit = mturk.createHIT(hitParams)
 print("Hit created  : "+ hit)
 
 //Report the results on the writeup
-    
